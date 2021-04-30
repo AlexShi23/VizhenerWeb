4426d9a986a7d3339447a950bb46f1f7621c0937
Aleksandr Shirokov <alieksandr_shirokov@list.ru>
2021-04-30 17:13:47 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using VizhenerWeb.Models;
+
+namespace VizhenerWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Update(VizhenerViewModel model, IFormFile file)
+        {
+            if (file == null) 
+            {
+                if (model == null || string.IsNullOrEmpty(model.Message))
+                    return RedirectToAction("Index");
+            }
+            else
+            {
+                if (file.Length > 0)
+                {
+                    try
+                    {
+                        model.Message = GetMessageFromFile(file);
+                    }
+                    catch (NotSupportedException)
+                    {
+                        model.Error = "Ошибка! Файл имеет расширение отличное от .docx!";
+                    }
+                    catch (Exception)
+                    {
+                        model.Error = "Возникла ошибка при чтении файла!";
+                    }
+                }
+                else
+                {
+                    model.Error = "Ошибка! Файл пустой!";
+                }
+            }
+            return View("Index", model);
+        }
+        private string GetMessageFromFile(IFormFile file)
+        {
+            string fileExtension = Path.GetExtension(file.FileName);
+            if (fileExtension == ".docx")
+            {
+                using (var fileStream = file.OpenReadStream())
+                {
+                    WordprocessingDocument wordDocument = WordprocessingDocument.Open(fileStream, false);
+                    Body body = wordDocument.MainDocumentPart.Document.Body;
+                    return body.InnerText;
+                }
+            }
+            else
+            { 
+                throw new NotSupportedException("Ошибка! Файл имеет расширение отличное от .docx!");
+            }
+        }
+        public IActionResult DownloadFile(VizhenerViewModel model)
+        {
+            try
+            {
+                MemoryStream mem = new MemoryStream();
+                WordprocessingDocument wordDocument = WordprocessingDocument.Create(mem, WordprocessingDocumentType.Document, true);
+
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                Paragraph para = body.AppendChild(new Paragraph());
+                Run run = para.AppendChild(new Run());
+                run.AppendChild(new Text(model.Result));
+                mainPart.Document.Save();
+                wordDocument.Close();
+                mem.Position = 0;
+
+                return File(mem, "application/docx", "result.docx");
+
+            }
+            catch (Exception)
+            {
+                model.Error = "Ошибка при создании файла";
+                return View("Index", model);
+            }
+        }
+    }
+}
---
+++ Models/VizhenerViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VizhenerWeb.Models
+{
+    public enum CipherMode { Encrypt, Decrypt }
+    public class VizhenerViewModel
+    {
+        public VizhenerViewModel()
+        {
+
+        }
+        public VizhenerViewModel(string message, string key, CipherMode mode, string error = null)
+        {
+            Message = message;
+            Key = key;
+            Mode = mode;
+            Error = error;
+        }
+        public string Message { get; set; }
+        public string Key { get; set; }
+        public CipherMode Mode { get; set; }
+        private string result;
+        public string Result
+        {
+            get
+            {
+                if (result == null)
+                {
+                    result = Cipher();
+                }
+                return result;
+            }
+            set
+            {
+                result = value;
+            }
+        }
+
+        public string Error { get; set; }
+
+        public static string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+        private string Cipher()
+        {
+            string result = "";
+            string fullKey = GetFullKey();
+
+            int keyIndex = 0;
+            if (Validate())
+            {
+                for (int i = 0; i < Message.Length; i++)
+                {
+                    if (alphabet.Contains(char.ToLower(Message[i])))
+                    {
+                        if (char.IsLower(Message[i]))
+                        {
+                            if (Mode == CipherMode.Encrypt)
+                                result += alphabet[(alphabet.IndexOf(Message[i]) + alphabet.IndexOf(fullKey[keyIndex])) % alphabet.Length];
+                            else
+                                result += alphabet[(alphabet.IndexOf(Message[i]) - alphabet.IndexOf(fullKey[keyIndex]) + alphabet.Length) % alphabet.Length];
+                        }
+                        else
+                        {
+                            if (Mode == CipherMode.Encrypt)
+                                result += char.ToUpper(alphabet[(alphabet.IndexOf(char.ToLower(Message[i])) + alphabet.IndexOf(fullKey[keyIndex])) % alphabet.Length]);
+                            else
+                                result += char.ToUpper(alphabet[(alphabet.IndexOf(char.ToLower(Message[i])) - alphabet.IndexOf(fullKey[keyIndex]) + alphabet.Length) % alphabet.Length]);
+                        }
+                        keyIndex++;
+                    }
+                    else
+                        result += Message[i];
+                }
+            }
+            return result;
+        }
+        public string GetFullKey()
+        {
+            string fullKey = "";
+            int length = 0;
+            for (int i = 0; i < Message.Length; i++)
+            {
+                if (alphabet.Contains(char.ToLower(Message[i])))
+                    length++;
+            }
+            for (int i = 0; i < length; i++)
+                fullKey += char.ToLower(Key[i % Key.Length]);
+            return fullKey;
+        }
+
+        public bool Validate()
+        {
+            if (Error != null) 
+                return false;
+            if (string.IsNullOrEmpty(Message))
+            {
+                Error = "Вы ввели пустое сообщение!";
+                return false;
+            }
+            else if (string.IsNullOrEmpty(Key))
+            {
+                Error = "Вы ввели пустой ключ!";
+                return false;
+            }
+            else if (!Key.All(c => alphabet.Contains(char.ToLower(c))))
+            {
+                Error = "Ключ должен содержать только буквы русского алфавита!";
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
